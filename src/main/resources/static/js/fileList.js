4916b557f26bb1abf68c91e775d37b713a51c15d
CURRENDAS\g.panowicz <grzegorz.panowicz@currenda.pl>
2020-02-24 11:45:12 +0100

    ostylowanie

--- src/main/resources/static/js/fileList.js
+++ src/main/resources/static/js/fileList.js
@@ -14,6 +14,79 @@ function getList(docType, idDoc) {
 	
 	request.onload = function () {
 		var response = JSON.parse(request.responseText);
+		
+		var col = [];
+        for (var i = 0; i < response.length; i++) {
+            for (var key in response[i]) {
+                if (col.indexOf(key) === -1) {
+                    col.push(key);
+                }
+            }
+        }
+        
+        var table = document.createElement("table");
+        
+        var tr = table.insertRow(-1);                   // TABLE ROW.
+
+        var th = document.createElement("th");      // TABLE HEADER.
+        th.classList.add("lp");
+        th.innerHTML = "Lp.";
+        tr.appendChild(th);
+        
+        th = document.createElement("th");      // TABLE HEADER.
+        th.innerHTML = "Nazwa";
+        tr.appendChild(th);
+        
+        th = document.createElement("th");      // TABLE HEADER.
+        th.classList.add("size");
+        th.innerHTML = "Rozmiar";
+        tr.appendChild(th);
+        
+        for (var i = 0; i < response.length; i++) {
+
+        	var file = response[i];
+        	
+            tr = table.insertRow(-1);
+
+            var tabCell = tr.insertCell(-1);
+            tabCell.innerHTML = i+1;
+            
+            tabCell = tr.insertCell(-1);
+            var link = 'http://localhost:8081/api/v1/downloadFile/' + file.uuid;
+            tabCell.href = link;
+            tabCell.innerHTML = '<a href="' + link + '">' + file.fileName + '</a>';
+            //tabCell.innerHTML = file.fileName; //response[i][col[6]];
+            
+            tabCell = tr.insertCell(-1);
+            var oriSize = Number(file.fileSize);
+			var sizeInKB = oriSize / 1024;
+			tabCell.innerHTML = sizeInKB.toFixed(2) + " kB";
+            
+        }
+
+        // FINALLY ADD THE NEWLY CREATED TABLE WITH JSON DATA TO A CONTAINER.
+        var divContainer = document.getElementById("showData");
+        divContainer.innerHTML = "";
+        divContainer.appendChild(table);
+        
+	}
+}
+
+
+function getList2(docType, idDoc) {
+	var request = new XMLHttpRequest();
+	//alert(docType);
+	//alert(idDoc);
+	//request.channel.loadFlags |= Components.interfaces.nsIRequest.LOAD_BYPASS_CACHE;
+	
+	request.open("GET", "http://localhost:8081/api/v1/files?type=" + docType + "&" + "linkedDocId=" + idDoc);
+	//request.setRequestHeader('Cache-Control', 'no-cache');
+	request.setRequestHeader("Cache-Control", "no-cache, no-store");
+	request.setRequestHeader('pragma', 'no-cache');
+	request.send();
+	
+	request.onload = function () {
+		var response = JSON.parse(request.responseText);
 		//alert(response.length);
 		
 		var api = document.getElementById('root');
